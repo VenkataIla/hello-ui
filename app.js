d8bca09531b015c7b1b4c9cf19288f7406317bff
venkata.ila <venkata.ila@thermofisher.com>
2021-05-31 17:41:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const request = require('request');
+
+// Constants
+const PORT = 3000;
+const HOST = '127.0.0.1';
+
+// App
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/hello', (req, res) => {
+    let options = {
+        url: "http://localhost:8080/hello",
+        method: 'GET',
+        json: true,
+        headers: req.defaultHeaders
+    };
+
+    request(options, function (error, response) {
+        if (!error) {
+            let data = response.body;
+            var dateformat = dateBuilder();
+            console.log(dateformat + " " + data);
+            res.send(dateformat + " " + data);
+        }
+    })
+});
+
+let dateBuilder = function(){
+    var date = new Date,
+    dateformat = [("0" + date.getDate()).slice(-2),
+    ("0" + (date.getMonth() + 1)).slice(-2),
+    date.getFullYear()].join('/') + ' ' +
+    [("0" + date.getHours()).slice(-2),
+    ("0" + (date.getMinutes())).slice(-2)].join(':');
+    return dateformat;
+}
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
